1f5810d4928d49f5225225e4d721a00c689e5b52
odieoo <odieooabdeen@gmail.com>
2024-11-08 21:39:01 +0300

    create pages and routes

--- src/App.js
+++ src/App.js
@@ -1,10 +1,27 @@
 
-
+import React from 'react'
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
+import Home from './pages/Home';
+import Profile from './pages/Profile';
+import SignIn from './pages/SignIn';
+import SignUp from './pages/SignUp';
+import Offers from './pages/Offers';
+import ForgotPassword from './pages/ForgotPassword';
 function App() {
   return (
     <>
-   <h1 className='text-2xl bg-red-400'>Hello world!</h1>
-    <h2> after adding github</h2>
+    <Router>
+    <Routes>
+        <Route path='/' element={<Home />} />
+        <Route path='/profile' element={<Profile />} />
+        <Route path='/sign-in' element={<SignIn />} />
+        <Route path='/sign-up' element={<SignUp />} />
+        <Route path='/forgot-password' element={<ForgotPassword />} />
+        <Route path='/offers' element={<Offers />} />
+
+   
+    </Routes>
+   </Router>
     </>
   );
 }
---
+++ src/pages/Home.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
+
+
+export default function Home() {
+  return (
+   <>
+  <h1> Home</h1>
+   
+   
+   </>
+  )
+}
